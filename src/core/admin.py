b87ee9a22798d33bbd72b8759ed1b2f30b60cc1d
Eldrian <eldrianayera710@gmail.com>
2025-09-14 21:44:35 +0800

    fix: add User model and fix non nullable fields

--- src/core/admin.py
+++ src/core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Course , User , Document , Flashcard
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Course)
+admin.site.register(Document)
+admin.site.register(Flashcard)
--- src/core/models.py
+++ src/core/models.py
@@ -6,21 +6,32 @@ class User(AbstractUser):
     pass
 
 class Course(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='courses')
     name = models.CharField(max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    course_note = models.TextField(max_length=50)
+    course_note = models.TextField(max_length=50, blank=True, null=True)
+    
+    def __str__(self):
+        return self.name
 
 
 class Document(models.Model):
     name = models.CharField(max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)
-    document_note = models.TextField(max_length=50)
-    summary = models.TextField(max_length=50, null=True)
+    document_note = models.TextField(max_length=50, blank=True)
+    summary = models.TextField(max_length=50, null=True, blank=True)
     course = models.ForeignKey(Course, related_name='documents', on_delete=models.CASCADE)
+    file = models.FileField(upload_to='documents/', null=True)
+    
+    def __str__(self):
+        return self.name
 
 class Flashcard(models.Model):
     question = models.CharField(max_length=50)
     answer = models.CharField(max_length=100)
     document = models.ForeignKey(Document, related_name='flashcards', on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.question
 
