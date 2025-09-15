6222831a4e92f83374b811133e733c77528cde0b
Eldrian <eldrianayera710@gmail.com>
2025-09-15 11:05:45 +0800

    build: add DRF simple jwt for auth

--- src/core/serializers.py
+++ src/core/serializers.py
@@ -1,28 +1,29 @@
 from rest_framework import serializers
 from .models import Course , User , Document , Flashcard
 
-class CourseSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Course
-        fields = '__all__'
-        
-class DocumentSerializer(serializers.ModelSerializer):
-    
-    class Meta:
-        model = Document
-        fields = '__all__'
-
-
 class FlashcardSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = Flashcard
+        fields = '__all__'
+        
+class DocumentSerializer(serializers.ModelSerializer):
+    flashcards = FlashcardSerializer(many=True)
+    
+    class Meta:
+        model = Document
         fields = '__all__'
 
 class UserSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = User
+        fields = '__all__'
+        
+class CourseSerializer(serializers.ModelSerializer):
+    documents = DocumentSerializer(many=True , read_only= True)
+
+    class Meta:
+        model = Course
         fields = '__all__'
 
