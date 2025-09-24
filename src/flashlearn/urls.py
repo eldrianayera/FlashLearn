72b7e50776789ea5193a02c9b7bf6576f283eec3
Eldrian <eldrianayera710@gmail.com>
2025-09-24 23:16:21 +0800

    build: update setting to prepare deployment;

--- src/flashlearn/urls.py
+++ src/flashlearn/urls.py
@@ -24,8 +24,10 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include('core.urls')),
-    path("__reload__/", include("django_browser_reload.urls")),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += [
+        path("__reload__/", include("django_browser_reload.urls")),
+    ]
