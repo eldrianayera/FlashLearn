9c84fba6c5f4f4732e2ed3464d7ad4b25070f308
Eldrian <eldrianayera710@gmail.com>
2025-09-25 10:47:46 +0800

    Revert "fix: fix wsgi"
    This reverts commit 8b0fda540e1a20958289e3675fdb21425dbc6f01.

--- src/flashlearn/wsgi.py
+++ src/flashlearn/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "flashlearnnnnn.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "flashlearn.settings")
 
 application = get_wsgi_application()
 
