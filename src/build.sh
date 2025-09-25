57242dbf4fb3b35fb3f0c29aa2b06c60c401becc
Eldrian <eldrianayera710@gmail.com>
2025-09-25 10:34:39 +0800

    fix: fix build.sh

--- src/build.sh
+++ src/build.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
-python install -r requirements.txt
+
+echo "BUILD START"
+
+# Create a virtual environment named 'venv' if it doesn't already exist
+python -m venv venv
+
+# Activate the virtual environment
+# Vercel might not use bash, so you can use the `source` command for bash or `.` for non-bash shells
+source venv/bin/activate
+
+# Install dependencies in the virtual environment
+pip install -r requirements.txt
+
+# Collect static files using the Python interpreter from the virtual environment
 python manage.py collectstatic --noinput
+
+echo "BUILD END"
+
+# [optional] Start the application here if running locally
+# python manage.py runserver
